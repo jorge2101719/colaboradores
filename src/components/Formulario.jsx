289ec082a8cd1f5a06b89a75fe7228bcaeea1240
jorge2101719 <rusosky01@gmail.com>
2023-08-28 02:36:23 -0400

    corrección en la lógica de validación de Formulario

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -6,18 +6,20 @@ import { Form, Row } from 'react-bootstrap'
 
 const Formulario = ({ setBaseDatos, baseDatos, setAlert }) => {
   // const [baseDatos, setBaseDatos] = useState(colaboradores)
-  // const [nombre, setNombre] = useState('')
-  // const [correo, setCorreo] = useState('')
-  const [input, setInput] = useState({nombre: '', correo: '', edad: '', cargo: '', telefono: ''})
-
-  const capturarInput = (e) => {
-    setInput({...input, [e.target.name]: e.target.value, [e.target.correo]: e.target.value, [e.target.edad]: e.target.value, [e.target.cargo]: e.target.value, [e.target.telefono]: e.target.value })
-  }
+  const [nombre, setNombre] = useState('')
+  const [correo, setCorreo] = useState('')
+  const [edad, setEdad] = useState('')
+  const [cargo, setCargo] = useState('')
+  const [telefono, setTelefono] = useState('')
+  // const [input, setInput] = useState({nombre: '', correo: '', edad: '', cargo: '', telefono: ''})
+  // const capturarInput = (e) => {
+    // setInput({...input, [e.target.name]: e.target.value, [e.target.correo]: e.target.value, [e.target.edad]: e.target.value, [e.target.cargo]: e.target.value, [e.target.telefono]: e.target.value })
+  // }
 
   const handleSubmit = (e) => {
     e.preventDefault()
 
-    if (e.target.name === '' || e.target.correo === '' || e.target.edad === '' || e.target.cargo === '' || e.target.telefono === '' ) {
+    if (nombre === '' || correo === '' || edad === '' || cargo === '' || telefono === '' ) {
       setAlert({
         error: true,
         msg: 'Se deben llenar todos los campos',
@@ -26,26 +28,31 @@ const Formulario = ({ setBaseDatos, baseDatos, setAlert }) => {
       return;
     }
 
-    setBaseDatos([...baseDatos, input])
-    
+    setBaseDatos([...baseDatos, {nombre: nombre, correo: correo, edad: edad, cargo: cargo, telefono: telefono}])
+
+    setNombre('')
+    setCorreo('')
+    setEdad('')
+    setCargo('')
+    setTelefono('')
+
     setAlert({
       error: false,
       msg: 'Información agregada',
       color: 'success'
     })
 
-    setInput('')
   }
 
   return (
     <div>
       <h1>Formulario</h1>
       <form onSubmit={(e) => handleSubmit(e)}>
-        <input onChange={capturarInput} name='nombre' key='nombre' placeholder='nombre'></input>
-        <input onChange={capturarInput} name='correo' key='correo' placeholder='correo'></input>
-        <input onChange={capturarInput}  name='edad' placeholder='edad'></input>
-        <input onChange={capturarInput}  name='cargo' placeholder='cargo'></input>
-        <input onChange={capturarInput}  name='telefono' placeholder='telefono'></input>
+        <input onChange={(e) => setNombre(e.target.value)} name='nombre' key='nombre' placeholder='nombre'></input>
+        <input onChange={(e) => setCorreo(e.target.value)} name='correo' key='correo' placeholder='correo'></input>
+        <input onChange={(e) => setEdad(e.target.value)}  name='edad' placeholder='edad'></input>
+        <input onChange={(e) => setCargo(e.target.value)}  name='cargo' placeholder='cargo'></input>
+        <input onChange={(e) => setTelefono(e.target.value)}  name='telefono' placeholder='telefono'></input>
         <Button type='submit' variant='primary' >Agregar</Button>
       </form>
 
